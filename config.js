fb00f11aea5b9e9dd62430772a9fff48a9bd4407
Thanvez91 <thanvez91@gmail.com>
2020-06-21 21:26:49 +0100

    site map issue

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: '#',
+  url: 'https://lumen.netlify.com',
   pathPrefix: '/',
   title: 'Thanvez Ahmed Portfolio',
   subtitle: 'Thanvez Ahmed Portfolio',
